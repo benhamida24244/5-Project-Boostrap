edbc1416014b54e4c40146535fc72c8e955b3faf
Benhamida Mohammed <mbenhamida136@gmail.com>
2024-08-06 19:28:28 +0100

    Navbar and Icon and Details Section

--- ebook-website/js/script.js
+++ ebook-website/js/script.js
@@ -1,1 +1,18 @@
-// Add custom JavaScript here
+
+function useScroll() {
+    const navbar = document.querySelector('.navbar');
+    window.addEventListener('scroll' , () => {
+    if (window.scrollY) {
+        navbar.classList.add('navbar-dark');
+        navbar.classList.remove('navbar-sticky');
+        navbar.classList.add('bg-dark');
+    }
+    else {
+        navbar.classList.remove('navbar-dark');
+        navbar.classList.remove('navbar-sticky');
+        navbar.classList.remove('bg-dark');
+    }
+    });    
+}
+
+document.addEventListener('DOMContentLoaded',useScroll);
